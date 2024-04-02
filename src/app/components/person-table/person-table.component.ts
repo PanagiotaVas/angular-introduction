4b304724b029519a078f5766ad178c22fa30778e
P. Vasileiou <vasileioupanagiota4@gmail.com>
2024-04-02 21:02:22 +0300

    for branch 9

--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, input } from '@angular/core';
-import { Person } from 'src/app/shared/interface/person';
+import { EPerson, Person } from 'src/app/shared/interface/person';
 
 @Component({
   selector: 'app-person-table',
@@ -10,6 +10,15 @@ import { Person } from 'src/app/shared/interface/person';
 })
 export class PersonTableComponent {
 
-  @Input() person: Person | undefined;
+  @Input() person: Person | EPerson | undefined;
 
+  isPerson() {
+    return this.person && 'address' in this.person
+        // ^^ if it is not undefined and has address
+  }
+
+  isEPerson() {
+    return this.person && 'education' in this.person
+        // ^^ if it is not undefined and has education
+  }
 }
