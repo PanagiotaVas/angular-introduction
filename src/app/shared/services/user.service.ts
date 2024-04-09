23fa11934316c4933f2f685d00c8f6f7e50f1462
P. Vasileiou <vasileioupanagiota4@gmail.com>
2024-04-09 21:08:14 +0300

    first steps of user registration

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { User } from '../interface/user';
+
+const API_URL = `${environment.apiURL}/user` 
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  http: HttpClient = inject(HttpClient)
+
+  registerUser(user: User) {
+    return this.http.post<{msg: string}>(`${API_URL}/register`, user)
+                            //^^ what the backend returns
+  }
+}
